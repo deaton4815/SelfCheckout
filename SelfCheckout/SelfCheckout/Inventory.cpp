a3ca8e6a3b9dbb0d60aac1dcc31c6bbc06859d37
Philip Deaton <philipdeaton826@gmail.com>
2024-03-09 17:15:04 -0500

    Inventory initialization updates

--- SelfCheckout/SelfCheckout/Inventory.cpp
+++ SelfCheckout/SelfCheckout/Inventory.cpp
@@ -7,64 +7,64 @@
 using namespace std;
 
 Inventory::Inventory()
-	: m_meat01ID{ "Meat01" }
-	, m_meat01Description{ "T-Bone Steak" }
-	, m_meat01Price{ 7.99 }
+	: m_meat01ID("Meat01")
+	, m_meat01Description("T-Bone Steak")
+	, m_meat01Price(7.99f)
 	, m_meat01(m_meat01ID, m_meat01Description, m_meat01Price)
 
-	, m_meat02ID{ "Meat02" }
-	, m_meat02Description{ "Tyson Fresh Chicken Wings" }
-	, m_meat02Price{ 10.00 }
+	, m_meat02ID("Meat02")
+	, m_meat02Description("Tyson Fresh Chicken Wings")
+	, m_meat02Price(10.00f)
 	, m_meat02(m_meat02ID, m_meat02Description, m_meat02Price)
 
-	, m_iceCream01ID{ "Icecream01" }
-	, m_iceCream01Description{ "Chocolate Ice Cream" }
-	, m_iceCream01Price{ 2.50 }
+	, m_iceCream01ID("Icecream01")
+	, m_iceCream01Description("Chocolate Ice Cream")
+	, m_iceCream01Price(2.50f)
 	, m_iceCream01(m_iceCream01ID, m_iceCream01Description, m_iceCream01Price)
 
-	, m_iceCream02ID{ "Icecream02" }
-	, m_iceCream02Description{ "Vanilla Ice Cream" }
-	, m_iceCream02Price{ 2.50 }
+	, m_iceCream02ID("Icecream02")
+	, m_iceCream02Description("Vanilla Ice Cream")
+	, m_iceCream02Price(2.50f)
 	, m_iceCream02(m_iceCream02ID, m_iceCream02Description, m_iceCream02Price)
 
-	, m_corn01ID{ "Corn01" }
-	, m_corn01Description{ "Fresh Sweet Corn" }
-	, m_corn01Price{ 2.00 }
+	, m_corn01ID("Corn01")
+	, m_corn01Description("Fresh Sweet Corn")
+	, m_corn01Price(2.00f)
 	, m_corn01(m_corn01ID, m_corn01Description, m_corn01Price)
 
-	, m_caseWaters01ID{ "Casewater" }
-	, m_caseWaters01Description{ "24 Bottles 16-Ox of Deer Park Water" }
-	, m_caseWaters01Price{ 4.99 }
+	, m_caseWaters01ID("Casewater")
+	, m_caseWaters01Description("24 Bottles 16-Ox of Deer Park Water")
+	, m_caseWaters01Price(4.99f)
 	, m_caseWater01(m_caseWaters01ID, m_caseWaters01Description, m_caseWaters01Price)
 
-	, m_potatoChips01ID{ "Potatochips01" }
-	, m_potatoChips01Description{ "Plain Potato Chips" }
-	, m_potatoChips01Price{ 2.00 }
+	, m_potatoChips01ID("Potatochips01")
+	, m_potatoChips01Description("Plain Potato Chips")
+	, m_potatoChips01Price(2.00f)
 	, m_potatoChips01(m_potatoChips01ID, m_potatoChips01Description, m_potatoChips01Price)
 
-	, m_potatoChips02ID{ "Potatochips02" }
-	, m_potatoChips02Description{ "Green Onion Potato Chips" }
-	, m_potatoChips02Price{ 2.00 }
+	, m_potatoChips02ID("Potatochips02")
+	, m_potatoChips02Description("Green Onion Potato Chips")
+	, m_potatoChips02Price(2.00f)
 	, m_potatoChips02(m_potatoChips02ID, m_potatoChips02Description, m_potatoChips02Price)
-	
-	, m_donuts01ID{ "Donuts01" }
-	, m_donuts01Description{ "Glazed Donutes One-Dozen" }
-	, m_donuts01Price{ 4.99 }
+
+	, m_donuts01ID("Donuts01")
+	, m_donuts01Description("Glazed Donutes One-Dozen")
+	, m_donuts01Price(4.99f)
 	, m_donuts01(m_donuts01ID, m_donuts01Description, m_donuts01Price)
 
-	, m_sausage01ID{ "Sausage01" }
-	, m_sausage01Description{ "8-Sausage Pack" }
-	, m_sausage01Price{ 4.99 }
+	, m_sausage01ID("Sausage01")
+	, m_sausage01Description("8-Sausage Pack")
+	, m_sausage01Price(4.99f)
 	, m_sausage01(m_sausage01ID, m_sausage01Description, m_sausage01Price)
 
-	, m_eggs01ID{ "Eggs01" }
-	, m_eggs01Description{ "Dozen Eggs" }
-	, m_eggs01Price{ 3.00 }
+	, m_eggs01ID("Eggs01")
+	, m_eggs01Description("Dozen Eggs")
+	, m_eggs01Price(3.00f)
 	, m_eggs01(m_eggs01ID, m_eggs01Description, m_eggs01Price)
 
-	, m_milk01ID{ "Milk01" }
-	, m_milk01Description{ "Gallon Milk" }
-	, m_milk01Price{ 4.00 }
+	, m_milk01ID("Milk01")
+	, m_milk01Description("Gallon Milk")
+	, m_milk01Price(4.00f)
 	, m_milk01(m_milk01ID, m_milk01Description, m_milk01Price)
 {}
 
--- SelfCheckout/SelfCheckout/Inventory.h
+++ SelfCheckout/SelfCheckout/Inventory.h
@@ -23,67 +23,67 @@ public:
 	Item getSausage01() const;
 	Item getEggs01() const;
 	Item getMilk01() const;
-	
+
 private:
 
-	const Item m_meat01;
 	const string m_meat01ID;
 	const string m_meat01Description;
 	const float m_meat01Price;
+	const Item m_meat01;
 
-	const Item m_meat02;
 	const string m_meat02ID;
 	const string m_meat02Description;
 	const float m_meat02Price;
+	const Item m_meat02;
 
-	const Item m_iceCream01;
 	const string m_iceCream01ID;
 	const string m_iceCream01Description;
 	const float m_iceCream01Price;
+	const Item m_iceCream01;
 
-	const Item m_iceCream02;
 	const string m_iceCream02ID;
 	const string m_iceCream02Description;
 	const float m_iceCream02Price;
+	const Item m_iceCream02;
 
-	const Item m_corn01;
 	const string m_corn01ID;
 	const string m_corn01Description;
 	const float m_corn01Price;
+	const Item m_corn01;
 
-	const Item m_caseWater01;
 	const string m_caseWaters01ID;
 	const string m_caseWaters01Description;
 	const float m_caseWaters01Price;
+	const Item m_caseWater01;
 
-	const Item m_potatoChips01;
 	const string m_potatoChips01ID;
 	const string m_potatoChips01Description;
 	const float m_potatoChips01Price;
+	const Item m_potatoChips01;
 
-	const Item m_potatoChips02;
 	const string m_potatoChips02ID;
 	const string m_potatoChips02Description;
 	const float m_potatoChips02Price;
+	const Item m_potatoChips02;
 
-	const Item m_donuts01;
 	const string m_donuts01ID;
 	const string m_donuts01Description;
 	const float m_donuts01Price;
+	const Item m_donuts01;
 
-	const Item m_sausage01;
 	const string m_sausage01ID;
 	const string m_sausage01Description;
 	const float m_sausage01Price;
+	const Item m_sausage01;
 
-	const Item m_eggs01;
 	const string m_eggs01ID;
 	const string m_eggs01Description;
 	const float m_eggs01Price;
+	const Item m_eggs01;
 
-	const Item m_milk01;
 	const string m_milk01ID;
 	const string m_milk01Description;
 	const float m_milk01Price;
+	const Item m_milk01;
 };
 
