d3fbfb047d2a37754d9a9ee2a45d5ca9532070ea
Philip Deaton <philipdeaton826@gmail.com>
2024-03-09 22:07:35 -0500

    Shopping cart Item vector

--- SelfCheckout/SelfCheckout/Cart.cpp
+++ SelfCheckout/SelfCheckout/Cart.cpp
@@ -1,1 +1,5 @@
 #include "Cart.h"
+
+void Cart::addItem(Item item) {
+	m_scannedItems.push_back(item);
+}
--- SelfCheckout/SelfCheckout/Cart.h
+++ SelfCheckout/SelfCheckout/Cart.h
@@ -1,5 +1,19 @@
 #pragma once
+
+#include <vector>
+
+#include "Item.h"
+
+using namespace std;
+
 class Cart
 {
-};
+public:
 
+	void addItem(Item);
+
+private:
+
+	vector<Item> m_scannedItems;
+	
+};
