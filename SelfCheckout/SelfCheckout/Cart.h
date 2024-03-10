c3691dd1aabefe13f0b92a555141a8cf1368974b
Philip Deaton <philipdeaton826@gmail.com>
2024-03-10 17:02:46 -0400

    Recording of cart subtotal and item numbers

--- SelfCheckout/SelfCheckout/Cart.h
+++ SelfCheckout/SelfCheckout/Cart.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <numeric>
 
 #include "Item.h"
 
@@ -13,14 +14,19 @@ public:
 
 	void addItem(const Item&);
 
+	vector<unsigned int> getItemNumbers();
 	vector<string> getItemIDs();
 	vector<string> getItemDescriptions();
 	vector<float> getItemPrices();
+	float getSubtotal();
 
 private:
 
 	vector<Item> m_scannedItems;
 
 	size_t m_countScannedItems{ 0 };
+	float m_subtotal{ 0.f };
+
+	void calculateSubtotal();
 
 };
