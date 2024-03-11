c06a0d3371a80dc66e353e947d1e0c17440df3e2
Philip Deaton <philipdeaton826@gmail.com>
2024-03-10 20:55:24 -0400

    Basic pay service class functionality

---
+++ SelfCheckout/SelfCheckout/PayService.h
@@ -0,0 +1,26 @@
+#pragma once
+class PayService
+{
+
+public:
+
+	void updatePrice(float);
+
+	float getTax();
+	float getTotal();
+	
+private:
+
+	float m_subtotal{ 0.f };
+	float m_tax{ 0.f };
+	const float m_taxRate{ 0.05f };
+	float m_total{ 0.f };
+
+	float calculateTax();
+	float calculateTotal();
+
+	void setSubtotal(float);
+	void setTax(float);
+	void setTotal(float);
+
+};
