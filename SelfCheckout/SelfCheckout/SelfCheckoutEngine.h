c6b19c656bc592b7328c20e3a0e9b38aa5e8892a
Philip Deaton <philipdeaton826@gmail.com>
2024-03-11 10:39:45 -0400

    Deleted comments from SelfChckoutEngine header

--- SelfCheckout/SelfCheckout/SelfCheckoutEngine.h
+++ SelfCheckout/SelfCheckout/SelfCheckoutEngine.h
@@ -17,27 +17,9 @@ private:
 	PayService m_scoPayService;
 	UserInterface m_scoUserInterface;
 
-	/*
-	vector<unsigned int> m_itemNumbers;
-	vector<string> m_itemIDs;
-	vector<string> m_itemDescriptions;
-	vector<float> m_itemPrices;
-
-	float m_subtotal{ 0.f };
-	float m_tax{ 0.f };
-	float m_total{ 0.f };
-	*/
-
 	void executeCustomerCheckout();
 	void executeItemSelection();
 	void scanItem(const int);
 	void updatePrice();
 	void displayCart();
-
-	/*
-	void updateCartInfo();
-
-	void updatePrice();
-	void updateSubtotal();
-	*/
 };
